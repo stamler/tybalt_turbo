9863b62c8eb8c702ff1547f5bf558f2adbe41591
Dean Stamler <dev@stamler.ca>
2024-06-11 15:17:32 -0400

    heavy validation work

--- app/go.mod
+++ app/go.mod
@@ -3,6 +3,8 @@ module tybalt
 go 1.22.3
 
 require (
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/labstack/echo/v5 v5.0.0-20230722203903-ec5b858dab61
 	github.com/pocketbase/dbx v1.10.1
 	github.com/pocketbase/pocketbase v0.22.13
 )
@@ -35,7 +37,6 @@ require (
 	github.com/fatih/color v1.17.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/ganigeorgiev/fexpr v0.4.1 // indirect
-	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
@@ -45,7 +46,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/labstack/echo/v5 v5.0.0-20230722203903-ec5b858dab61 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
--- app/hooks/hooks.go
+++ app/hooks/hooks.go
@@ -3,8 +3,12 @@ package hooks
 import (
 	"encoding/json"
 	"log"
+	"regexp"
 
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/models"
 	"github.com/pocketbase/pocketbase/tools/list"
@@ -22,9 +26,9 @@ import (
 // record that are not allowed to be set based on the value of the record's
 // time_type property. This is intended to reduce round trips to the database
 // and to ensure that the record is in a valid state before it is created or
-// updated. It is called by validateTimeEntry to reduce the number of fields
+// updated. It is called by ProcessTimeEntry to reduce the number of fields
 // that need to be validated.
-func cleanTimeEntry(app *pocketbase.PocketBase, timeEntryRecord *models.Record) error {
+func cleanTimeEntry(app *pocketbase.PocketBase, timeEntryRecord *models.Record) (string, error) {
 	timeTypeId := timeEntryRecord.GetString("time_type")
 
 	// Load the allowed fields for the time_type from the time_types collection in
@@ -32,7 +36,7 @@ func cleanTimeEntry(app *pocketbase.PocketBase, timeEntryRecord *models.Record) 
 	// of strings.
 	timeTypeRecord, findError := app.Dao().FindRecordById("time_types", timeTypeId)
 	if findError != nil {
-		return findError
+		return "", findError
 	}
 
 	// Get the allowed fields from the time_type record with type assertion
@@ -42,7 +46,7 @@ func cleanTimeEntry(app *pocketbase.PocketBase, timeEntryRecord *models.Record) 
 	// use json.Unmarshal to convert the JSON array to a Go slice of strings
 	if unmarshalError := json.Unmarshal(allowedFieldsJson, &allowedFields); unmarshalError != nil {
 		log.Fatalf("Error unmarshalling JSON: %v", unmarshalError)
-		return unmarshalError
+		return "", unmarshalError
 	}
 
 	// Certain fields are always allowed to be set. We add them to the list of
@@ -58,36 +62,188 @@ func cleanTimeEntry(app *pocketbase.PocketBase, timeEntryRecord *models.Record) 
 		}
 	}
 
-	return nil
+	return timeTypeRecord.GetString("code"), nil
 }
 
-// The ValidateTimeEntry function is used to validate the time_entry record
+// cross-field validation is performed in this function. If it is already set,
+// the uid property is checked against the uid of the authenticated user. An
+// error is thrown if the uids do not match. Otherwise the uid property is set
+// to the uid of the authenticated user.
+func validateTimeEntry(timeEntryRecord *models.Record, timeTypeCode string) error {
+
+	// implement this with go-ozzo/ozzo-validation because it is already used by PocketBase
+	isWorkTime := list.ExistInSlice(timeTypeCode, []string{"R", "RT"})
+	jobIsPresent := timeEntryRecord.Get("job") != ""
+	isOTO := timeTypeCode == "OTO"
+	hoursRequired := list.ExistInSlice(timeTypeCode, []string{"OB", "OH", "OP", "OS", "OV", "RB"})
+	hoursProhibited := list.ExistInSlice(timeTypeCode, []string{"OR", "OW", "OTO"})
+
+	err := validation.Errors{
+		"division": validation.Validate(timeEntryRecord.Get("division"), validation.When(isWorkTime, validation.Required.Error("division is required when time_type is R or RT"))),
+		"hours": validation.Validate(timeEntryRecord.Get("hours"),
+			validation.When(isWorkTime && jobIsPresent, validation.In(0).Error("hours must be 0 when a job is provided")),
+			validation.When((isWorkTime && !jobIsPresent) || hoursRequired, validation.Required.Error("hours must be at least 0.5"), validation.Min(0.5).Error("hours must be 0.5 or more"), validation.Max(18.0).Error("hours must be 18 or less")),
+			validation.When(hoursProhibited, validation.In(0).Error("hours must be 0 when time_type is OR, OW or OTO")),
+		),
+		"meals_hours":           validation.Validate(timeEntryRecord.Get("meals_hours"), validation.Max(3).Error("meals_hours must be less than 3")),
+		"job_hours":             validation.Validate(timeEntryRecord.Get("job_hours"), validation.When(jobIsPresent, validation.Min(0.5).Error("job_hours must be at least 0.5 when a job is provided"), validation.Max(18.0).Error("job_hours must be 18 or less")).Else(validation.In(0).Error("job_hours must be 0 when a job is not provided"))),
+		"description":           validation.Validate(timeEntryRecord.Get("description"), validation.Required, validation.Length(5, 0).Error("description must be at least 5 characters")),
+		"work_record":           validation.Validate(timeEntryRecord.Get("work_record"), validation.When(jobIsPresent, validation.Match(regexp.MustCompile("^[FKQ][0-9]{2}-[0-9]{3,}(-[0-9]+)?$")).Error("work_record must be in the correct format"))),
+		"payout_request_amount": validation.Validate(timeEntryRecord.Get("payout_request_amount"), validation.When(isOTO, validation.Required.Error("payout_request_amount is required when time_type is OTO")).Else(validation.Min(0).Exclusive().Error("payout_request_amount must be greater than 0"))),
+	}.Filter()
+
+	return err
+
+	// The following is the firestore rules function that we are trying to
+	// replicate here. It is a simple example of cross-field validation.
+	/*
+		function validTimeEntry() {
+			return
+
+				// prevent salaried staff from submitting an RB entry
+				(
+					get(/databases/$(database)/documents/Profiles/$(request.auth.uid)).data.salary == false ||
+					( get(/databases/$(database)/documents/Profiles/$(request.auth.uid)).data.salary == true && newDoc().timetype != "RB" )
+				) &&
+
+				// when job provided, job & division exist in db,
+				// jobDescription and client are present, timetype is 'R' or 'RT'
+				// hours field is missing
+				(
+					(
+						// if a category property is specified, it must be a string and that
+						// string must be in the categories list of the corresponding job
+						// Document. if the category property is not specified, the job must not
+						// have any categories
+						(
+							(isMissing("category") && !("categories" in get(/databases/$(database)/documents/Jobs/$(newDoc().job)).data)) ||
+							(newDoc().category is string && newDoc().category in get(/databases/$(database)/documents/Jobs/$(newDoc().job)).data.categories)
+						)
+					) ||
+					isMissing("job")
+				) &&
+
+				// when provided, division exists in db, Time Type is 'R' or 'RT'
+				// when division is missing, Time Type is not 'R' and is not 'RT'
+				(
+					( isInCollection("division","Divisions") &&
+						(newDoc().timetype == "R" || newDoc().timetype == "RT") ) ||
+					(isMissing("division") &&
+						(newDoc().timetype != "R" && newDoc().timetype != "RT") )
+				) &&
+
+				// at least one hours type is provided OR the timetype is "OR", "OW" or "OTO" or "RB"
+				(
+					(
+						newDoc().timetype != "OW" &&
+						newDoc().timetype != "OR" &&
+						newDoc().timetype != "RB" &&
+						newDoc().timetype != "OTO" &&
+						!(newDoc().keys().hasAll(["payoutRequestAmount"])) && // payoutRequestAmount is not defined
+						(
+							(isPositiveMultipleOfPointFiveUnderEighteen("jobHours") && !(newDoc().timetype in ["OR", "OW"])) ||
+							(isPositiveMultipleOfPointFiveUnderEighteen("hours") && !(newDoc().timetype in ["OR", "OW"])) ||
+							(isPositiveMultipleOfPointFiveUnderEighteen("mealsHours") && !(newDoc().timetype in ["OR", "OW"]))
+						)
+					) ||
+					(newDoc().timetype in ["OR", "OW"] && newDoc().keys().hasOnly(["date", "timetype", "timetypeName", "uid", "weekEnding"])) ||
+					(newDoc().timetype == "OTO" && newDoc().keys().hasOnly(["date", "timetype", "timetypeName", "uid", "weekEnding", "payoutRequestAmount"]) && newDoc().payoutRequestAmount is number && newDoc().payoutRequestAmount > 0) ||
+					(newDoc().timetype == "RB" && isPositiveMultipleOfPointFive("hours"))
+				) &&
+
+				// validate presence and length of description for Regular Hours and Training
+				// also ensure workDescription does not contain jobNumbers of the format
+				// XX-YYY where XX is a number between 15 and 40 and YYY is a 3-digit
+				// number between 001 and 999
+				(
+					(newDoc().workDescription.size() > 4 && !newDoc().workDescription.matches('.*(1[5-9]|2[0-9]|3[0-9]|40)-([0-9]{3}).*') && newDoc().timetype in ["R", "RT"]) || !(newDoc().timetype in ["R", "RT"])
+				) &&
+
+				// ensure absence of description for Banking, Payout, Off and Off Rotation
+				(
+					( isMissing("workDescription") && newDoc().timetype in ["RB", "OTO", "OR", "OW"]) || !(newDoc().timetype in ["RB", "OTO", "OW", "OR"])
+				) &&
+
+				// if timetype is "RB", only hours is provided and is a positive number
+				(
+					(isPositiveMultipleOfPointFive("hours") && newDoc().timetype == "RB" && newDoc().keys().hasOnly(["date", "timetype", "timetypeName", "uid", "weekEnding", "hours"])) ||
+					newDoc().timetype != "RB"
+				) &&
+
+				// when provided, jobHours require an existing job
+				(
+					( isPositiveMultipleOfPointFiveUnderEighteen("jobHours") && isInCollection("job", "Jobs") ) ||
+					isMissing("jobHours")
+				) &&
+
+				// when provided, mealsHours requires either jobHours or hours to be present
+				(
+					(
+						isPositiveMultipleOfPointFiveUnderEighteen("mealsHours") &&
+						(isPositiveMultipleOfPointFiveUnderEighteen("hours") || isPositiveMultipleOfPointFiveUnderEighteen("jobHours"))
+					) ||
+					isMissing("mealsHours")
+				) &&
+
+				// The total of jobHours and mealsHours and hours doesn't exceed 18 unless timetype is RB
+				(
+					newDoc().timetype != "RB" &&
+					valueOrZero("mealsHours") + valueOrZero("jobHours") + valueOrZero("hours") <= 18 ||
+					newDoc().timetype == "RB"
+				) &&
+				// when provided workrecord requires an existing job
+				(
+					( hasValidWorkrecord() && isInCollection("job", "Jobs") ) ||
+					isMissing("workrecord")
+				);
+		}
+	*/
+
+	// return nil
+}
+
+// The ProcessTimeEntry function is used to validate the time_entry record
 // before it is created or updated. A lot of the work is done by PocketBase
 // itself so this is for cross-field validation. If the time_entry record is
 // invalid this function throws an error explaining which field(s) are invalid
 // and why.
-func ValidateTimeEntry(app *pocketbase.PocketBase, record *models.Record) error {
-	// set properties to nil if they are not allowed to be set based on the
-	// time_type
-	if cleanErr := cleanTimeEntry(app, record); cleanErr != nil {
-		return cleanErr
+func ProcessTimeEntry(app *pocketbase.PocketBase, record *models.Record, context echo.Context) error {
+	// get the auth record from the context
+	authRecord := context.Get(apis.ContextAuthRecordKey).(*models.Record)
+
+	// If the uid property is not equal to the authenticated user's uid, return an
+	// error.
+	if record.GetString("uid") != authRecord.Id {
+		return apis.NewApiError(400, "uid property must be equal to the authenticated user's id", map[string]validation.Error{})
 	}
 
-	// Perform validation here
+	// set properties to nil if they are not allowed to be set based on the
+	// time_type
+	timeTypeCode, cleanErr := cleanTimeEntry(app, record)
+	if cleanErr != nil {
+		return apis.NewBadRequestError("Error cleaning time_entry record", cleanErr)
+	}
+
+	// perform the validation for the time_entry record. In this step we also
+	// write the uid property to the record so that we can validate it against the
+	if validationErr := validateTimeEntry(record, timeTypeCode); validationErr != nil {
+		return apis.NewBadRequestError("Validation error", validationErr)
+	}
+
 	return nil
 }
 
 func AddHooks(app *pocketbase.PocketBase) {
 	// hooks for time_entries model
 	app.OnRecordBeforeCreateRequest("time_entries").Add(func(e *core.RecordCreateEvent) error {
-		if err := ValidateTimeEntry(app, e.Record); err != nil {
+		if err := ProcessTimeEntry(app, e.Record, e.HttpContext); err != nil {
 			// return the error to the client
 			return err
 		}
 		return nil
 	})
 	app.OnRecordBeforeUpdateRequest("time_entries").Add(func(e *core.RecordUpdateEvent) error {
-		if err := ValidateTimeEntry(app, e.Record); err != nil {
+		if err := ProcessTimeEntry(app, e.Record, e.HttpContext); err != nil {
 			// return the error to the client
 			return err
 		}
