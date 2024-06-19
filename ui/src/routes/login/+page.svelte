9e94616a6f5bb649f1dc0c0279859ec48eed8508
Dean Stamler <dev@stamler.ca>
2024-06-19 16:33:05 -0400

    svelte 5 syntax

--- ui/src/routes/login/+page.svelte
+++ ui/src/routes/login/+page.svelte
@@ -7,7 +7,7 @@
   <div class="flex flex-col items-center align-center rounded w-96 p-4 bg-neutral-100 border">
     <h1 class="text-4xl font-bold">Welcome to Tybalt</h1>
     <p class="text-lg">Login with Microsoft to get started</p>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold m-2 py-2 px-4 rounded" on:click="{ authStore.loginWithMicrosoft }">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold m-2 py-2 px-4 rounded" onclick="{ authStore.loginWithMicrosoft }">
       Login
     </button>
   </div>
