39108771b658bdf57c5045b63e9cf11a2c225391
Dean Stamler <dev@stamler.ca>
2024-06-19 14:04:49 -0400

    fix layout to show not logged in

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -12,9 +12,9 @@
 
 <header class="w-screen h-10 bg-neutral-700 text-white px-3 flex justify-between">
   <h1 class="h-full flex flex-col justify-center">Tybalt</h1>
-  {#if authStoreValue !== null && authStoreValue?.isValid }
+  <div class="h-full">
+    {#if authStoreValue !== null && authStoreValue?.isValid }
     <!-- user is logged in -->
-    <div class="h-full">
       <span class="flex items-center h-full gap-2">
         <span>{ authStoreValue?.model?.email }</span>
         <button class="bg-blue-500 hover:bg-blue-700 text-white text-sm py-1 px-2 rounded" on:click={ authStore.logout }>
@@ -22,10 +22,11 @@
             <path fill-rule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z" clip-rule="evenodd" />
           </svg>
         </button>
-        <span>Not logged in</span>
       </span>
-    </div>
-  {/if}
+    {:else}
+      <span class="flex items-center h-full gap-2">Not logged in</span>
+    {/if}
+  </div>
 </header>
 <main>
   <slot />
