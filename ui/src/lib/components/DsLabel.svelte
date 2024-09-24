d8a58e95b0c52bdb8a11b5127c0c2e7c21a2587b
Dean Stamler <dev@stamler.ca>
2024-09-24 12:02:33 -0400

    Include an optional title property in Dslabel to show tool tips

--- ui/src/lib/components/DsLabel.svelte
+++ ui/src/lib/components/DsLabel.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
-  let { children, color } = $props<{
+  let { children, color, title } = $props<{
     children: Snippet<[string]>;
     color: string;
+    title?: string;
   }>();
 </script>
 
 <span
   class="flex items-center gap-1 rounded border border-{color}-300 bg-{color}-100 px-2 py-1 text-sm text-{color}-500"
+  {title}
 >
   {@render children()}
 </span>
