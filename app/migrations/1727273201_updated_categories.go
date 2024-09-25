6939ffefb5cc6958ca104b51c00b7644eaafb10f
Dean Stamler <dev@stamler.ca>
2024-09-25 10:09:46 -0400

    Update category deletion rules by adding check for referencing purchase orders

---
+++ app/migrations/1727273201_updated_categories.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/tools/types"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("nrwhbwowokwu6cr")
+		if err != nil {
+			return err
+		}
+
+		collection.DeleteRule = types.Pointer("@request.auth.id != \"\" &&\n@request.auth.user_claims_via_uid.cid.name ?= 'job' &&\n// prevent deletion of categories if there are referencing time_entries\n@collection.time_entries.category != id &&\n// prevent deletion of categories if there are referencing purchase orders\n@collection.purchase_orders.category != id\n// prevent deletion of categories if there are referencing expenses\n//@collection.expenses.category != id")
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("nrwhbwowokwu6cr")
+		if err != nil {
+			return err
+		}
+
+		collection.DeleteRule = types.Pointer("@request.auth.id != \"\" &&\n@request.auth.user_claims_via_uid.cid.name ?= 'job' &&\n// prevent deletion of categories if there are referencing time_entries\n@collection.time_entries.category != id\n// prevent deletion of categories if there are referencing purchase orders\n//@collection.purchase_orders.category != id\n// prevent deletion of categories if there are referencing expenses\n//@collection.expenses.category != id")
+
+		return dao.SaveCollection(collection)
+	})
+}
