f8d311487f610adcfa18f88aecfde07cfc281115
Dean Stamler <dev@stamler.ca>
2024-10-10 10:52:12 -0400

    Enhance deletion rules and update category-job relationship
    - Update deletion rules for jobs to prevent removal when referenced by time entries, purchase orders, or expenses
    - Modify deletion rules for categories to prevent removal when referenced by time entries, purchase orders, or expenses
    - Enable cascade delete for the job field in the categories collection
    - Simplify job and client list components to use globalStore

---
+++ app/migrations/1728571760_updated_jobs.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/tools/types"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		collection.DeleteRule = types.Pointer("@request.auth.id != \"\" &&\n@request.auth.user_claims_via_uid.cid.name ?= 'admin' &&\n\n// prevent deletion of jobs if there are referencing time_entries\n@collection.time_entries.job != id &&\n\n// prevent deletion of jobs if there are referencing purchase orders\n@collection.purchase_orders.job != id &&\n\n// prevent deletion of jobs if there are referencing expenses\n@collection.expenses.job != id")
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		collection.DeleteRule = types.Pointer("@request.auth.id != \"\" &&\n@request.auth.user_claims_via_uid.cid.name ?= 'admin' &&\n\n// prevent deletion of jobs if there are referencing time_entries\n@collection.time_entries.job != id &&\n\n// prevent deletion of jobs if there are referencing expenses\n@collection.expenses.job != id")
+
+		return dao.SaveCollection(collection)
+	})
+}
--- ui/src/routes/jobs/list/+page.svelte
+++ ui/src/routes/jobs/list/+page.svelte
@@ -1,53 +1,35 @@
 <script lang="ts">
   import DsList from "$lib/components/DSList.svelte";
-  import type { PageData } from "./$types";
-  import { pb } from "$lib/pocketbase";
-  import type { JobsResponse } from "$lib/pocketbase-types";
+  import { globalStore } from "$lib/stores/global";
   import DsActionButton from "$lib/components/DSActionButton.svelte";
-  let { data }: { data: PageData } = $props();
-
-  let items = $state(data.items);
-
-  async function del(id: string): Promise<void> {
-    // return immediately if items is not an array
-    if (!Array.isArray(items)) return;
-
-    try {
-      await pb.collection("jobs").delete(id);
-
-      // remove the item from the list
-      items = items.filter((item) => item.id !== id);
-    } catch (error: any) {
-      alert(error.data.message);
-    }
-  }
 </script>
 
-{#snippet anchor({ number }: JobsResponse)}{number}{/snippet}
-{#snippet headline({ description }: JobsResponse)}{description}{/snippet}
-{#snippet byline({ expand }: JobsResponse)}
-  <span>{expand?.client.name}</span>
-{/snippet}
-{#snippet line1({ expand }: JobsResponse)}
-  <span class="flex gap-1">
-    {#each expand?.categories_via_job as category}
-      <span class="rounded-sm border border-cyan-100 bg-cyan-50 px-1">{category.name}</span>
-    {/each}
-  </span>
-{/snippet}
-{#snippet actions({ id }: JobsResponse)}
-  <DsActionButton action="/jobs/{id}/edit" icon="mdi:pencil" title="Edit" color="blue" />
-  <DsActionButton action="/details/{id}" icon="mdi:more-circle" title="More Details" color="blue" />
-  <DsActionButton action={() => del(id)} icon="mdi:delete" title="Delete" color="red" />
-{/snippet}
-
-<!-- Show the list of items here -->
-<DsList
-  items={items as JobsResponse[]}
-  search={true}
-  {anchor}
-  {headline}
-  {byline}
-  {line1}
-  {actions}
-/>
+<DsList items={$globalStore.jobs} search={true}>
+  {#snippet anchor({ number })}{number}{/snippet}
+  {#snippet headline({ description })}{description}{/snippet}
+  {#snippet byline({ expand })}
+    <span>{expand?.client.name}</span>
+  {/snippet}
+  {#snippet line1({ expand })}
+    <span class="flex gap-1">
+      {#each expand?.categories_via_job as category}
+        <span class="rounded-sm border border-cyan-100 bg-cyan-50 px-1">{category.name}</span>
+      {/each}
+    </span>
+  {/snippet}
+  {#snippet actions({ id })}
+    <DsActionButton action="/jobs/{id}/edit" icon="mdi:pencil" title="Edit" color="blue" />
+    <DsActionButton
+      action="/details/{id}"
+      icon="mdi:more-circle"
+      title="More Details"
+      color="blue"
+    />
+    <DsActionButton
+      action={() => globalStore.deleteItem("jobs", id)}
+      icon="mdi:delete"
+      title="Delete"
+      color="red"
+    />
+  {/snippet}
+</DsList>
