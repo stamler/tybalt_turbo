ddf4a15932602535c0e3263a9439071487e6aadf
Dean Stamler <dev@stamler.ca>
2025-04-02 16:21:07 -0400

    add the "notification_types" multiple select field to the profiles collection.

---
+++ app/migrations/1743625192_updated_profiles.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("glmf9xpnwgpwudm")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "select2676255945",
+			"maxSelect": 2,
+			"name": "notification_types",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "select",
+			"values": [
+				"email_text",
+				"email_html"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("glmf9xpnwgpwudm")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("select2676255945")
+
+		return app.Save(collection)
+	})
+}
