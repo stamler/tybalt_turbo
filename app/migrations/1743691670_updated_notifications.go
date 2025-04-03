f071f87f5ab75ee49710a65b7a3e0f2ea69f5738
Dean Stamler <dev@stamler.ca>
2025-04-03 10:57:14 -0400

    change notifications status values to 				"pending", "inflight", "sent", "error"

---
+++ app/migrations/1743691670_updated_notifications.go
@@ -0,0 +1,63 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2301922722")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"hidden": false,
+			"id": "select2063623452",
+			"maxSelect": 1,
+			"name": "status",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "select",
+			"values": [
+				"pending",
+				"inflight",
+				"sent",
+				"error"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2301922722")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"hidden": false,
+			"id": "select2063623452",
+			"maxSelect": 1,
+			"name": "status",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "select",
+			"values": [
+				"pending",
+				"sent",
+				"failed"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
