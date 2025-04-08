f3978b2363df3f73e11da3969261c86a58e3631d
Dean Stamler <dev@stamler.ca>
2025-04-08 16:59:54 -0400

    add data column (JSON) to notifications table to provide extra data to templates in notifications

---
+++ app/migrations/1744145895_updated_notifications.go
@@ -0,0 +1,41 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2301922722")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"hidden": false,
+			"id": "json2918445923",
+			"maxSize": 0,
+			"name": "data",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "json"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2301922722")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("json2918445923")
+
+		return app.Save(collection)
+	})
+}
