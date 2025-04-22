f4b1a637021089193333450da363441878097e96
Dean Stamler <dev@stamler.ca>
2025-04-22 09:27:18 -0400

    add fn_agreement and status columns to jobs

---
+++ app/migrations/1745328319_updated_jobs.go
@@ -0,0 +1,64 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "bool1047208438",
+			"name": "fn_agreement",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"hidden": false,
+			"id": "select2063623452",
+			"maxSelect": 1,
+			"name": "status",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "select",
+			"values": [
+				"Active",
+				"Closed",
+				"Cancelled",
+				"Awarded",
+				"Not Awarded"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("bool1047208438")
+
+		// remove field
+		collection.Fields.RemoveById("select2063623452")
+
+		return app.Save(collection)
+	})
+}
