0537a40ed10a1e9eb18779110ce52d7a6fb0330a
Dean Stamler <dev@stamler.ca>
2025-04-22 09:05:40 -0400

    add alternate_manager column to jobs

---
+++ app/migrations/1745327118_updated_jobs.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(6, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "_pb_users_auth_",
+			"hidden": false,
+			"id": "relation2937628849",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "alternate_manager",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("relation2937628849")
+
+		return app.Save(collection)
+	})
+}
