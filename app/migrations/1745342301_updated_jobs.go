760b0dc77ac86c481677bc92e974fc5c600ee648
Dean Stamler <dev@stamler.ca>
2025-04-22 13:18:50 -0400

    add divisions column to jobs table

---
+++ app/migrations/1745342301_updated_jobs.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(13, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "3esdddggow6dykr",
+			"hidden": false,
+			"id": "relation1542800728",
+			"maxSelect": 999,
+			"minSelect": 0,
+			"name": "field",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("relation1542800728")
+
+		return app.Save(collection)
+	})
+}
