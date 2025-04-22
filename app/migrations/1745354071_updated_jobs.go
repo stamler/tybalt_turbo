579aaeab2189762e4931a196d1659e79bc231208
Dean Stamler <dev@stamler.ca>
2025-04-22 16:35:12 -0400

    add categories column to jobs

---
+++ app/migrations/1745354071_updated_jobs.go
@@ -0,0 +1,41 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(15, []byte(`{
+			"hidden": false,
+			"id": "json989021800",
+			"maxSize": 0,
+			"name": "categories",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "json"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("yovqzrnnomp0lkx")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("json989021800")
+
+		return app.Save(collection)
+	})
+}
