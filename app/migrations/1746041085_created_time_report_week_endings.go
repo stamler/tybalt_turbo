7574af99de58629ad432c64d1b92dc2415f6bb94
Dean Stamler <dev@stamler.ca>
2025-04-30 15:33:46 -0400

    feat: add time_report_week_endings view
    Adds a PocketBase view to retrieve distinct week_ending dates
    from committed time_sheets, ordered descendingly. This simplifies
    fetching available dates for payroll reports.

---
+++ app/migrations/1746041085_created_time_report_week_endings.go
@@ -0,0 +1,70 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 0,
+					"min": 0,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "_clone_sbpV",
+					"max": 0,
+					"min": 0,
+					"name": "week_ending",
+					"pattern": "^\\d{4}-\\d{2}-\\d{2}$",
+					"presentable": true,
+					"primaryKey": false,
+					"required": true,
+					"system": false,
+					"type": "text"
+				}
+			],
+			"id": "pbc_317365225",
+			"indexes": [],
+			"listRule": "@request.auth.id != \"\"",
+			"name": "time_report_week_endings",
+			"system": false,
+			"type": "view",
+			"updateRule": null,
+			"viewQuery": "SELECT\n  MIN(id) as id,\n  week_ending\nFROM time_sheets\nWHERE committed != ''\nGROUP BY week_ending\nORDER BY week_ending DESC;",
+			"viewRule": "@request.auth.id != \"\""
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_317365225")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
