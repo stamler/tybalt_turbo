4ed35dcaa5ebff76c32273a77ad081bd62d7a2d2
Dean Stamler <dev@stamler.ca>
2025-07-14 17:26:09 -0400

    Refactor clients query to use LEFT JOINs instead of correlated subqueries
    Replace correlated subqueries for contacts and job counts with LEFT JOINs
    and CTEs to improve query performance and avoid N+1 query issues.

--- app/routes/clients.sql
+++ app/routes/clients.sql
@@ -1,27 +1,30 @@
--- Aggregate clients with their contacts to avoid PocketBase's N+1 expand on client_contacts_via_client
+-- Aggregate clients with their contacts and job counts using LEFT JOINs to avoid N+1 queries
 SELECT
   c.id,
   c.name,
-  COALESCE(
-    (
-      SELECT json_group_array(
-        json_object(
-          'id', cc.id,
-          'given_name', cc.given_name,
-          'surname', cc.surname,
-          'email', cc.email
-        )
-      )
-      FROM client_contacts cc
-      WHERE cc.client = c.id
-    ),
-    '[]'
-  ) AS contacts_json,
-  (
-    SELECT COUNT(*)
-    FROM jobs j
-    WHERE j.client = c.id
-  ) AS referencing_jobs_count
+  COALESCE(cc_agg.contacts_json, '[]') AS contacts_json,
+  COALESCE(j_count.referencing_jobs_count, 0) AS referencing_jobs_count
 FROM clients c
+LEFT JOIN (
+  SELECT 
+    client,
+    json_group_array(
+      json_object(
+        'id', id,
+        'given_name', given_name,
+        'surname', surname,
+        'email', email
+      )
+    ) AS contacts_json
+  FROM client_contacts
+  GROUP BY client
+) cc_agg ON cc_agg.client = c.id
+LEFT JOIN (
+  SELECT 
+    client,
+    COUNT(*) AS referencing_jobs_count
+  FROM jobs
+  GROUP BY client
+) j_count ON j_count.client = c.id
 WHERE ({:id} IS NULL OR {:id} = '' OR c.id = {:id})
 ORDER BY c.name; 
