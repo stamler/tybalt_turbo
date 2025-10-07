a64aba536159ff84904cb72998f788ae13b22f58
Dean Stamler <dev@stamler.ca>
2025-10-07 14:16:06 -0400

    fix #50

--- ui/src/routes/login/+page.svelte
+++ ui/src/routes/login/+page.svelte
@@ -11,9 +11,13 @@
   });
 </script>
 
-<!-- The login box, centered vertically and horizontally using flex -->
-<div class="flex h-screen flex-col items-center justify-center">
-  <div class="flex w-96 flex-col items-center rounded border bg-neutral-100 p-4 align-middle">
+<!-- The login box, top-aligned on mobile and centered on larger screens -->
+<div class="flex min-h-screen flex-col items-center justify-start md:justify-center">
+  <!-- Logo -->
+  <img src="/logo.svg" alt="Tybalt Logo" class="m-4 h-32 w-32 select-none md:h-40 md:w-40" />
+  <div
+    class="flex w-full max-w-sm flex-col items-center rounded border bg-neutral-100 p-4 align-middle sm:max-w-md"
+  >
     <h1 class="text-4xl font-bold">Welcome to Tybalt</h1>
     <p class="text-lg">Login with Microsoft to get started</p>
     <button
