41826266d5dfb33401942fbb6ee2ef8cc3aab885
Dean Stamler <dev@stamler.ca>
2025-11-10 11:07:01 -0500

    feat: updates the job_time_allocations collection to include a unique index on job and division fields to prevent duplicates

---
+++ app/migrations/1762790765_updated_job_time_allocations.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3457251721")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": [
+				"CREATE UNIQUE INDEX ` + "`" + `idx_7yDEv2P8Xg` + "`" + ` ON ` + "`" + `job_time_allocations` + "`" + ` (\n  ` + "`" + `job` + "`" + `,\n  ` + "`" + `division` + "`" + `\n)"
+			]
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3457251721")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": []
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
