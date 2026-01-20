6aea12b718c1d437de1a1c67c4d087541295e2c2
Dean Stamler <dev@stamler.ca>
2026-01-20 09:57:34 -0500

    Add 'active' boolean field  admin_profiles
    This commit introduces a new migration that adds an 'active' boolean field to the admin profiles collection. The migration includes functionality to add and remove the field as needed. Additionally, the test database has been updated to reflect these changes.

---
+++ app/migrations/1768920907_updated_admin_profiles.go
@@ -0,0 +1,40 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("zc850lb2wclrr87")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(22, []byte(`{
+			"hidden": false,
+			"id": "bool1260321794",
+			"name": "active",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("zc850lb2wclrr87")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("bool1260321794")
+
+		return app.Save(collection)
+	})
+}
