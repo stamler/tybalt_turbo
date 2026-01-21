c835aa85c791716556acc655406eb45964748f69
Dean Stamler <dev@stamler.ca>
2026-01-21 17:30:53 -0500

    Add rate_sheets collection for versioned billing rate definitions
    These rate_sheets are assignable to jobs and will be referenced by rate_sheet_entries records

---
+++ app/migrations/1769034474_created_rate_sheets.go
@@ -0,0 +1,126 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text790377260",
+					"max": 0,
+					"min": 0,
+					"name": "effective_date",
+					"pattern": "^\\d{4}-\\d{2}-\\d{2}$",
+					"presentable": false,
+					"primaryKey": false,
+					"required": true,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1579384326",
+					"max": 0,
+					"min": 0,
+					"name": "name",
+					"pattern": "",
+					"presentable": true,
+					"primaryKey": false,
+					"required": true,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "number1835210188",
+					"max": null,
+					"min": 0,
+					"name": "revision",
+					"onlyInt": true,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "bool1260321794",
+					"name": "active",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "bool"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_126575313",
+			"indexes": [
+				"CREATE UNIQUE INDEX ` + "`" + `idx_dHsmkOUme0` + "`" + ` ON ` + "`" + `rate_sheets` + "`" + ` (\n  ` + "`" + `name` + "`" + `,\n  ` + "`" + `revision` + "`" + `\n)"
+			],
+			"listRule": "@request.auth.id != \"\"",
+			"name": "rate_sheets",
+			"system": false,
+			"type": "base",
+			"updateRule": null,
+			"viewRule": "@request.auth.id != \"\""
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_126575313")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
