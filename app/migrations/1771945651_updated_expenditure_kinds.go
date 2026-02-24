e78b14e324333fa4a6f458dd2713635e3183c3bb
Dean Stamler <dev@stamler.ca>
2026-02-24 10:20:56 -0500

    Add migration to update expenditure kind field for job allowance
    - Introduced migration 1771945651 to modify the "allow_job" field in the "pbc_675944091" collection.
    - The field is set to non-required in the initial migration and required in the rollback.
    - Updated uid_replacements.csv to include a new UID mapping.
    - Adjusted binary database file due to changes in the schema.

---
+++ app/migrations/1771945651_updated_expenditure_kinds.go
@@ -0,0 +1,50 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_675944091")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "bool1771101200",
+			"name": "allow_job",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_675944091")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "bool1771101200",
+			"name": "allow_job",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
