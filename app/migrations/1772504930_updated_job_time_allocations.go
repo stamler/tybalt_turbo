675b9f69017e29267ffc62d9f5a63e35908fac1b
Dean Stamler <dev@stamler.ca>
2026-03-02 21:32:25 -0500

    fix(schema): cascade delete job_time_allocations when job is deleted

---
+++ app/migrations/1772504930_updated_job_time_allocations.go
@@ -0,0 +1,58 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3457251721")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"cascadeDelete": true,
+			"collectionId": "yovqzrnnomp0lkx",
+			"hidden": false,
+			"id": "relation4225294584",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "job",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3457251721")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "yovqzrnnomp0lkx",
+			"hidden": false,
+			"id": "relation4225294584",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "job",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
